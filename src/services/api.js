b793eab06dde9e5e278cd15ce857397695ec4229
OlehKotov <mail.oleg456@gmail.com>
2024-04-08 15:56:59 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,48 +1,49 @@
 import axios from "axios";
 
-const options = {
-  headers: {
-    Authorization:
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3"
+axios.defaults.headers = {
+  Authorization:
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjZjMTA5ZjUxNjA1N2JkYTYyZjkyZTU3YTJlM2IzZCIsInN1YiI6IjY2MGZkYzY3ODhjNjU5MDE3YzljZTRhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6-zrAow6l_ehri9Th2OKE6zz_DBa9rFdNjzahThjsP0",
-  },
-};
+}
+// const options = {
+//   headers: {
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjZjMTA5ZjUxNjA1N2JkYTYyZjkyZTU3YTJlM2IzZCIsInN1YiI6IjY2MGZkYzY3ODhjNjU5MDE3YzljZTRhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6-zrAow6l_ehri9Th2OKE6zz_DBa9rFdNjzahThjsP0",
+//   },
+// };
 
 export const requestTrendingMovies = async () => {
   const { data } = await axios.get(
-    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
-    options
+    "/trending/movie/day?language=en-US"
   );
   return data.results;
 };
 
 export const requestMovieDetailsById = async (movieId) => {
   const { data } = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
-    options
+    `/movie/${movieId}?language=en-US`
   );
   return data;
 };
 
 export const requestMovieCastById = async (movieId) => {
   const { data } = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`,
-    options
+    `/movie/${movieId}/credits?language=en-US`
   );
   return data.cast;
 };
 
 export const requestMovieReviewById = async (movieId) => {
   const { data } = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/reviews?language=en-US&page=1`,
-    options
+    `/movie/${movieId}/reviews?language=en-US&page=1`
   );
   return data.results;
 };
 
 export const requestMovieBySearch = async (query) => {
   const { data } = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
-    options
+    `/search/movie?query=${query}&include_adult=false&language=en-US&page=1`
   );
   return data.results;
 };
